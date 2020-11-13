cc06cd23b8c1e389df9d56a94beec68b2e18b66c
Tiago Barufi <barufi@gmail.com>
2020-11-13 14:48:05 -0300

    multiple-deploy

--- configure.sh
+++ configure.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-mkdir nginx
 
+if [[ ! -d "nginx" ]]; then
+     mkdir nginx
+fi
 while read line; do export "$line"; done < .env
 echo "START"
+
+while read line; do echo "$line"; done < .env
+
+echo ${CONTAINER_NAME}
 LANGUAGES=$(tr \' " " <<<"$LANGUAGES")
 sed -e "s/PROTOCOL/${PROTOCOL}/g" \
      -e "s/HOST_NAME/${HOST_NAME}/g" \
@@ -19,9 +25,9 @@ scripts.template.js > web/dist/scripts/scripts.698e6068.js
 cp driver-app.conf nginx/driver.conf
 sed -i -e "s/HOST_NAME/${HOST_NAME}/g" \
 	-e "s,    root \/opt\/web\/dist,    root $WINDSHAFT_FILES\/web\/dist,g" \
--e "s/driver-django/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' driver-django)/g" \
--e "s/driver-celery/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' driver-celery)/g" \
--e "s/windshaft/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' windshaft)/g" \
+-e "s/driver-django/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' driver-django-${CONTAINER_NAME})/g" \
+-e "s/driver-celery/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' driver-celery-${CONTAINER_NAME})/g" \
+-e "s/windshaft/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' windshaft-${CONTAINER_NAME})/g" \
 nginx/driver.conf
 
 #docker exec driver-nginx sed -i -e "s/HOST_NAME/${HOST_NAME}/g" /etc/nginx/conf.d/driver-app.conf
@@ -33,8 +39,8 @@ else
      echo "HTTPS"
  #    docker exec driver-nginx certbot
 fi
-docker exec driver-django ./manage.py collectstatic --noinput
-docker exec driver-django ./manage.py migrate
+docker exec "driver-django-${CONTAINER_NAME}" ./manage.py collectstatic --noinput
+docker exec "driver-django-${CONTAINER_NAME}" ./manage.py migrate
 
 sudo mv nginx/driver.conf /etc/nginx/sites-enabled/driver-sp.conf
 sudo service nginx restart
