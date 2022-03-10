a6a4a2210b791fb94205c0247ea64c6393d99bbf
Tiago Barufi <barufi@gmail.com>
2022-03-10 09:03:52 -0300

    array join translated

---
+++ angular/driver/src/app/input/arrayjoin.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Pipe({
+    name: 'arrayjoin',
+    pure: false
+})
+export class ArrayJoinPipe implements PipeTransform {
+    constructor(private translateService: TranslateService) {
+}
+    transform(all: string[]): string {
+        return all.map(k=>this.translateService.instant(k)).join(", ")
+    }
+}
